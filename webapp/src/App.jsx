844b2bad8cd22d7550485b49fd377d32869a3bc4
shravanip419 <shravanip319@gmail.com>
2025-08-17 21:47:34 +0530

    Update App.jsx

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -10,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/leaderboard" element={<Leaderboard />} />
+        
        </Routes>
     </Router>
   );
