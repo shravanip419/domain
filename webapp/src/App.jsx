92422d22400df80117b987124de8bc19bd900872
shravanip419 <shravanip319@gmail.com>
2025-08-17 21:53:53 +0530

    app.jsx

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Leaderboard from "./pages/Leaderboard";
 import Home from "./Pages/Home";
+import Team from "./pages/Team";
 
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/Team" element={<Team />} />
         
        </Routes>
     </Router>
